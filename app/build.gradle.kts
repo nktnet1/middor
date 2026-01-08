9abbee204490465a3bf9fb36e970d0e840f05cdc
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-01-08 22:09:34 +1100

    0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "org.nktnet.middor"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
     }
 
     buildFeatures {
